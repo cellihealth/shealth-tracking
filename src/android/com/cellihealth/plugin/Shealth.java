dcdf4b55d474a113c6bd1e1b69947d8bfd1680e7
Adi Prasetyo <prasetyo.adi@Adis-MacBook-Pro.local>
2017-02-06 17:46:44 +0700

    Add Cordova Plugin for Samsung Health Tracking

---
+++ src/android/com/cellihealth/plugin/Shealth.java
@@ -0,0 +1,213 @@
+package com.cellihealth.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+import com.cellihealth.cellihealth.MainActivity;
+
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthDataService;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionKey;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionResult;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionType;
+import com.samsung.android.sdk.healthdata.HealthResultHolder;
+
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Shealth extends CordovaPlugin {
+    public Set<PermissionKey> mKeySet;
+    private HealthDataStore mStore;
+    private HealthConnectionErrorResult mConnError;
+    private StepCountReporter mReporter;
+    public CallbackContext globalCallbackContext;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("steps")) {
+            // this.steps();
+            String startDate = args.getJSONObject(0).getString("startDate");
+            String endDate   = args.getJSONObject(0).getString("endDate");
+
+            this.stepsSHealth(startDate, endDate);
+
+            globalCallbackContext = callbackContext;
+            return true;
+        }else if(action.equals("connect")){
+            this.connect();
+            globalCallbackContext = callbackContext;
+            return true;
+        }
+        return false;
+    }
+    
+    private void connect() {
+        mKeySet = new HashSet<PermissionKey>();
+        mKeySet.add(new PermissionKey(HealthConstants.StepCount.HEALTH_DATA_TYPE, PermissionType.READ));
+        HealthDataService healthDataService = new HealthDataService();
+        try {
+            healthDataService.initialize(MainActivity.mInstance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Create a HealthDataStore instance and set its listener
+        mStore = new HealthDataStore(MainActivity.mInstance, mConnectionListener);
+        // Request the connection to the health data store
+        mStore.connectService();
+    }
+    
+
+    @Override
+    public void onDestroy() {
+        mStore.disconnectService();
+        super.onDestroy();
+    }
+
+
+    private final HealthDataStore.ConnectionListener mConnectionListener = new HealthDataStore.ConnectionListener() {
+
+        @Override
+        public void onConnected() {
+            Log.d(MainActivity.APP_TAG, "Health data service is connected.");
+            HealthPermissionManager pmsManager = new HealthPermissionManager(mStore);
+            mReporter = new StepCountReporter(mStore, Shealth.this);
+
+            try {
+                // Check whether the permissions that this application needs are acquired
+                Map<PermissionKey, Boolean> resultMap = pmsManager.isPermissionAcquired(mKeySet);
+                
+                if (resultMap.containsValue(Boolean.FALSE)) {
+                    // Request the permission for reading step counts if it is not acquired
+                    pmsManager.requestPermissions(mKeySet, MainActivity.mInstance).setResultListener(mPermissionListener);
+                } else {
+                    // Get the current step count and display it
+                    Log.d(MainActivity.APP_TAG, "COUNT THE STEPS!");
+                    mReporter.start();
+                }
+            } catch (Exception e) {
+                Log.e(MainActivity.APP_TAG, e.getClass().getName() + " - " + e.getMessage());
+                Log.e(MainActivity.APP_TAG, "Permission setting fails.");
+                globalCallbackContext.error("Permission setting fails.");
+            }
+        }
+
+        @Override
+        public void onConnectionFailed(HealthConnectionErrorResult error) {
+            Log.d(MainActivity.APP_TAG, "Health data service is not available.");
+            globalCallbackContext.error("Health data service is not available.");
+        }
+
+        @Override
+        public void onDisconnected() {
+            Log.d(MainActivity.APP_TAG, "Health data service is disconnected.");
+            globalCallbackContext.error("Health data service is disconnected.");
+        }
+    };
+    
+    private final HealthResultHolder.ResultListener<PermissionResult> mPermissionListener =
+            new HealthResultHolder.ResultListener<PermissionResult>() {
+
+        @Override
+        public void onResult(PermissionResult result) {
+            Log.d(MainActivity.APP_TAG, "Permission callback is received.");
+            Map<PermissionKey, Boolean> resultMap = result.getResultMap();
+
+            if (resultMap.containsValue(Boolean.FALSE)) {
+                Log.e(MainActivity.APP_TAG, "NOT CONNECTED YET");
+                showPermissionAlarmDialog();
+            } else {
+                Log.d(MainActivity.APP_TAG, "COUNT THE STEPS!");
+                mReporter.start();
+            }
+        }
+    };
+    
+    private void showPermissionAlarmDialog() {
+    	globalCallbackContext.error("All permissions should be acquired");
+//        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.mInstance);
+//        alert.setTitle("Notice");
+//        alert.setMessage("All permissions should be acquired");
+//        alert.setPositiveButton("OK", null);
+//        alert.show();
+    }
+
+    private void showConnectionFailureDialog(HealthConnectionErrorResult error) {
+
+        //AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.mInstance);
+        mConnError = error;
+        String message = "Connection with S Health is not available";
+
+        if (mConnError.hasResolution()) {
+            switch(error.getErrorCode()) {
+            case HealthConnectionErrorResult.PLATFORM_NOT_INSTALLED:
+                message = "Please install S Health";
+                break;
+            case HealthConnectionErrorResult.OLD_VERSION_PLATFORM:
+                message = "Please upgrade S Health";
+                break;
+            case HealthConnectionErrorResult.PLATFORM_DISABLED:
+                message = "Please enable S Health";
+                break;
+            case HealthConnectionErrorResult.USER_AGREEMENT_NEEDED:
+                message = "Please agree with S Health policy";
+                break;
+            default:
+                message = "Please make S Health available";
+                break;
+            }
+        }
+        globalCallbackContext.error(message);
+
+//        alert.setMessage(message);
+//
+//        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                if (mConnError.hasResolution()) {
+//                    mConnError.resolve(MainActivity.mInstance);
+//                }
+//            }
+//        });
+//
+//        if (error.hasResolution()) {
+//            alert.setNegativeButton("Cancel", null);
+//        }
+//
+//        alert.show();
+    }
+
+    
+    private void steps() {
+    	Log.d(MainActivity.APP_TAG, "steps");
+    	mReporter.start();
+    }
+
+    private void stepsSHealth(String startDate, String endDate) {
+        Log.d(MainActivity.APP_TAG, "steps");
+        mReporter.startReadStepCount(startDate, endDate);
+     }
+
+    public void drawStepCount(JSONArray count){
+    	Log.d(MainActivity.APP_TAG, "drawStepCount: " + count);
+    	globalCallbackContext.success(count);
+    }
+
+
+}
---
+++ src/android/com/cellihealth/plugin/StepCountReporter.java
@@ -0,0 +1,216 @@
+package com.cellihealth.plugin;
+
+import com.cellihealth.cellihealth.MainActivity;
+
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthDataObserver;
+import com.samsung.android.sdk.healthdata.HealthDataResolver;
+import com.samsung.android.sdk.healthdata.HealthDataResolver.Filter;
+import com.samsung.android.sdk.healthdata.HealthDataResolver.ReadRequest;
+import com.samsung.android.sdk.healthdata.HealthDataResolver.ReadResult;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthResultHolder;
+import com.samsung.android.sdk.healthdata.HealthDeviceManager;
+
+
+import android.database.Cursor;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import org.apache.cordova.LOG;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class StepCountReporter {
+    private final HealthDataStore mStore;
+    public Shealth sHealth = null;
+
+    public StepCountReporter(HealthDataStore store, Shealth shealth) {
+        mStore = store;
+        sHealth = shealth;
+    }
+
+    public void start() {
+        // Register an observer to listen changes of step count and get today step count
+        // HealthDataObserver.addObserver(mStore, HealthConstants.StepCount.HEALTH_DATA_TYPE, mObserver);
+        readStepCount();
+    }
+
+    public void startReadStepCount(String startDate, String endDate) {
+        HealthDataResolver resolver = new HealthDataResolver(mStore, null);
+
+        // Set time range from start time of today to the current time
+        long startTime = setToMiliseconds(startDate);
+        long endTime = setToMiliseconds(endDate);
+        Filter filter = Filter.and(Filter.greaterThanEquals(HealthConstants.StepCount.START_TIME, startTime),
+                Filter.lessThanEquals(HealthConstants.StepCount.START_TIME, endTime));
+
+        HealthDataResolver.ReadRequest request = new ReadRequest.Builder()
+                .setDataType(HealthConstants.StepCount.HEALTH_DATA_TYPE)
+                .setProperties(new String[]{
+                        HealthConstants.StepCount.COUNT,
+                        HealthConstants.StepCount.START_TIME,
+                        HealthConstants.StepCount.END_TIME,
+                        HealthConstants.StepCount.TIME_OFFSET,
+                        HealthConstants.StepCount.DEVICE_UUID
+                })
+                .setFilter(filter)
+                .build();
+
+        try {
+            resolver.read(request).setResultListener(mListener);
+
+            Log.e(MainActivity.APP_TAG, "Getting step count success.");
+        } catch (Exception e) {
+            Log.e(MainActivity.APP_TAG, e.getClass().getName() + " - " +  e.getMessage());
+            Log.e(MainActivity.APP_TAG, "Getting step count fails.");
+            sHealth.globalCallbackContext.error("Getting step count fails.");
+        }
+    }
+
+    // Read the today's step count on demand
+    private void readStepCount() {
+        HealthDataResolver resolver = new HealthDataResolver(mStore, null);
+
+        // Set time range from start time of today to the current time
+        long startTime = getStartTimeOfToday() - ( 7 * 24 * 60 * 60 * 1000 );
+        long endTime = System.currentTimeMillis();
+        Filter filter = Filter.and(Filter.greaterThanEquals(HealthConstants.StepCount.START_TIME, startTime),
+                                   Filter.lessThanEquals(HealthConstants.StepCount.START_TIME, endTime));
+
+        HealthDataResolver.ReadRequest request = new ReadRequest.Builder()
+                                                        .setDataType(HealthConstants.StepCount.HEALTH_DATA_TYPE)
+                                                        .setProperties(new String[]{
+                                                                HealthConstants.StepCount.COUNT,
+                                                                HealthConstants.StepCount.START_TIME,
+                                                                HealthConstants.StepCount.END_TIME,
+                                                                HealthConstants.StepCount.TIME_OFFSET,
+                                                                HealthConstants.StepCount.DEVICE_UUID
+                                                        })
+                                                        .setFilter(filter)
+                                                        .build();
+
+        try {
+            resolver.read(request).setResultListener(mListener);
+        } catch (Exception e) {
+            Log.e(MainActivity.APP_TAG, e.getClass().getName() + " - " + e.getMessage());
+            Log.e(MainActivity.APP_TAG, "Getting step count fails.");
+            sHealth.globalCallbackContext.error("Getting step count fails.");
+        }
+    }
+
+    private long getStartTimeOfToday() {
+        Calendar today = Calendar.getInstance();
+
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
+
+        return today.getTimeInMillis();
+    }
+
+    private final HealthResultHolder.ResultListener<ReadResult> mListener = new HealthResultHolder.ResultListener<ReadResult>() {
+        @Override
+        public void onResult(ReadResult result) {
+            String resultSet = "[";
+
+            Cursor c = null;
+
+            HealthDeviceManager deviceManager = new HealthDeviceManager(mStore);
+
+            try {
+                c = result.getResultCursor();
+
+                Log.d(MainActivity.APP_TAG, "Column Names" + Arrays.toString(c.getColumnNames()));
+
+                if (c != null) {
+                    String deviceName = "";
+                    String deviceManufacturer = "";
+                    String deviceModel = "";
+                    byte[] dataText = null;
+
+                    while (c.moveToNext()) {
+
+                        deviceName = deviceManager.getDeviceByUuid(c.getString(c.getColumnIndex(HealthConstants.StepCount.DEVICE_UUID))).getCustomName();
+                        deviceManufacturer = deviceManager.getDeviceByUuid(c.getString(c.getColumnIndex(HealthConstants.StepCount.DEVICE_UUID))).getManufacturer();
+                        deviceModel = deviceManager.getDeviceByUuid(c.getString(c.getColumnIndex(HealthConstants.StepCount.DEVICE_UUID))).getModel();
+
+                        if (deviceName == null) {
+                            deviceName = "";
+                        }
+
+                        if (deviceManufacturer == null) {
+                            deviceManufacturer = "";
+                        }
+
+                        if (deviceModel == null) {
+                            deviceModel = "";
+                        }
+
+                        resultSet += "{ offset: " +
+                                c.getString(c.getColumnIndex(HealthConstants.StepCount.TIME_OFFSET)) + ", start:" +
+                                c.getString(c.getColumnIndex(HealthConstants.StepCount.START_TIME)) + ", end:" +
+                                c.getString(c.getColumnIndex(HealthConstants.StepCount.END_TIME)) + ", step:" +
+                                c.getInt(c.getColumnIndex(HealthConstants.StepCount.COUNT)) + ", deviceName:\"" +
+                                deviceName + "\", deviceManufacturer:\"" + deviceManufacturer + "\", deviceModel:\"" + deviceModel + "\"}, ";
+                    }
+
+                } else {
+                    Log.d(MainActivity.APP_TAG, "The cursor is null.");
+                }
+            }
+            catch(Exception e) {
+                Log.e(MainActivity.APP_TAG, e.getClass().getName() + " - " + e.getMessage());
+            }
+            finally {
+                if (c != null) {
+                    c.close();
+                }
+            }
+            resultSet += "]";
+
+            Log.d(MainActivity.APP_TAG, "Steps Result: " + resultSet);
+            try{
+                JSONArray p = new JSONArray(resultSet);
+                sHealth.drawStepCount(p);
+            }catch (JSONException e){
+                LOG.d(MainActivity.APP_TAG, "JSONException on resultSet.");
+            }
+        }
+    };
+
+    private final HealthDataObserver mObserver = new HealthDataObserver(null) {
+
+        // Update the step count when a change event is received
+        @Override
+        public void onChange(String dataTypeName) {
+            Log.d(MainActivity.APP_TAG, "Observer receives a data changed event");
+            readStepCount();
+        }
+    };
+
+    private long setToMiliseconds(String str) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        long miliseconds = 0;
+
+        try {
+            Date date = format.parse(str);
+            miliseconds = date.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return miliseconds;
+    }
+
+
+}
